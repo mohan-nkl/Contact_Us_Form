606c621abcdc85ba025997cd5d25fdb028658891
Mohan Lakkoju <nagakamesh1997@gmail.com>
2026-05-30 16:29:27 +0530

    Added some spaces to DAO classes for a better readability.

--- src/main/java/daos/AdminDAO.java
+++ src/main/java/daos/AdminDAO.java
@@ -1,5 +1,51 @@
 package daos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import models.Admin;
+import utils.DBConnection;
+
 public class AdminDAO {
 
+	public Admin findAdminByUsername(String username) throws SQLException {
+		
+		String sql = "SELECT id, username, password_hash FROM admins WHERE username=?";
+		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			
+			connection = DBConnection.getConnection();
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, username);
+			resultSet = preparedStatement.executeQuery();
+			
+			if (resultSet.next()) {
+				Admin admin = new Admin();
+				admin.setAdminId(resultSet.getInt("id"));
+				admin.setName(resultSet.getString("username"));
+				admin.setHashedPassword(resultSet.getString("password_hash"));
+				return admin;
+			}
+			
+			return null;
+		}
+		finally {
+			
+			if (resultSet != null) {
+				resultSet.close();
+			}
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+			if (connection != null) {
+				connection.close();
+			}
+		}
+	}
 }
--- src/main/java/daos/ContactRequestDAO.java
+++ src/main/java/daos/ContactRequestDAO.java
@@ -1,5 +1,113 @@
 package daos;
 
-public class ContactRequestDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import models.ContactRequest;
+import utils.DBConnection;
+
+public class ContactRequestDAO {
+	
+	public void saveRequest(ContactRequest request) throws SQLException {
+		
+		String sql = "INSERT INTO contact_requests (full_name, email, message) " + 
+						"VALUES(?, ?, ?)";
+		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		
+		try {
+			
+			connection = DBConnection.getConnection();
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, request.getFullName());
+			preparedStatement.setString(2, request.getEmail());
+			preparedStatement.setString(3, request.getMessage());
+			preparedStatement.executeUpdate();
+		}
+		finally {
+			
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+			if (connection != null) {
+				connection.close();
+			}
+		}
+	}
+	
+	
+	public List<ContactRequest> getAllRequests() throws SQLException {
+		
+		String sql = "SELECT id, full_name, email, message, status, created_at FROM contact_requests ORDER BY created_at DESC";
+		
+		List<ContactRequest> requests = new ArrayList<>();
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			
+			connection = DBConnection.getConnection();
+			preparedStatement = connection.prepareStatement(sql);
+			resultSet = preparedStatement.executeQuery();
+			
+			while (resultSet.next()) {
+				
+				ContactRequest request = new ContactRequest();
+                request.setRequestId(resultSet.getInt("id"));
+                request.setFullName(resultSet.getString("full_name"));
+                request.setEmail(resultSet.getString("email"));
+                request.setMessage(resultSet.getString("message"));
+                request.setStatus(resultSet.getString("status"));
+                request.setCreatedAt(resultSet.getTimestamp("created_at").toLocalDateTime());
+                requests.add(request);
+			}
+			
+			return requests;
+		}
+		finally {
+			
+			if (resultSet != null) {
+				resultSet.close();
+			}
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+			if (connection != null) {
+				connection.close();
+			}
+		}
+		
+	}
+	
+	public void updateRequestStatus(int id, String status) throws SQLException {
+		
+		String sql = "UPDATE contact_requests SET status = ? WHERE id = ?";
+		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		
+		try {
+			
+			connection = DBConnection.getConnection();
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, status);
+			preparedStatement.setInt(2, id);
+			preparedStatement.executeUpdate();
+		}
+		finally {
+			
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+			if (connection != null) {
+				connection.close();
+			}
+		}
+	}
 }
